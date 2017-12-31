6ee70cea61e6d921b26724c5cdb5db771d431737
Alex Chen <alexbiz@foxmail.com>
2017-12-31 17:09:34 +0800

    test

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,8 +1,3 @@
-variable "ARM_CLIENT_ID" {}
-variable "ARM_CLIENT_SECRET" {}
-variable "ARM_SUBSCRIPTION_ID" {}
-variable "ARM_TENANT_ID" {}
-
 variable "location" {
   description = "The location where resources are created"
   default     = "East US"
