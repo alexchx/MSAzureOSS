2ca4c8d597d35096f6df50e6304a3bd946c5ff80
Alex Chen <alexbiz@foxmail.com>
2017-12-31 19:46:57 +0800

    vmss

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,2 +1,19 @@
-variable "location" {}
-variable "resource_group_name" {}
+variable "location" {
+  description = "The location where resources are created"
+  default     = ""
+}
+
+variable "resource_group_name" {
+  description = "The name of the resource group in which the resources are created"
+  default     = ""
+}
+
+variable "application_port" {
+  description = "The port that you want to expose to the external load balancer"
+  default     = 80
+}
+
+variable "admin_password" {
+  description = "Default password for admin"
+  default     = "Passw@rd"
+}
