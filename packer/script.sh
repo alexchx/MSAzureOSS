839df53c7258db39ee41cf4829ee70e3a89937c4
Alex Chen <alexbiz@foxmail.com>
2018-01-02 20:14:15 +0800

    test

--- packer/script.sh
+++ packer/script.sh
@@ -1,17 +1,24 @@
 apt-get update
 apt-get upgrade -y
+
 # https://tecadmin.net/install-tomcat-9-on-ubuntu/
 # https://askubuntu.com/questions/777342/how-to-install-tomcat-9
+# install jdk
 apt-get -y install openjdk-8-jdk
+
+# install tomcat
 cd /opt
 wget http://www-us.apache.org/dist/tomcat/tomcat-9/v9.0.2/bin/apache-tomcat-9.0.2.tar.gz
 tar xzf apache-tomcat-9.0.2.tar.gz
 mv apache-tomcat-9.0.2 tomcat9
+
+# configure environment variables
 echo 'export CATALINA_HOME="/opt/tomcat9"' >> /etc/environment
 echo 'export JAVA_HOME="/usr/lib/jvm/java-8-openjdk-amd64"' >> /etc/environment
 echo 'export JRE_HOME="/usr/lib/jvm/java-8-openjdk-amd64/jre"' >> /etc/environment
 source ~/.bashrc
 
+# download code and package into .war
 cd /tmp
 git clone https://github.com/alexchx/MSAzureOSS
 cd ./MSAzureOSS/helloworld/WebContent
@@ -23,6 +30,7 @@ cd /opt/tomcat9
 # https://stackoverflow.com/questions/4756039/how-to-change-the-port-of-tomcat-from-8080-to-80
 # https://dzone.com/articles/running-tomcat-port-80-user
 # http://2ality.blogspot.com/2010/07/running-tomcat-on-port-80-in-user.html
+# change default port to 80 from 8080
 sed -i 's/Connector port="8080"/Connector port="80"/g' ./conf/server.xml
 apt-get install authbind
 touch /etc/authbind/byport/80
